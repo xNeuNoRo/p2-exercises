2742b2248c92994fcfbb49fedcb95d126ac37aca
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-03-01 19:44:49 -0400

    feat: Add ProfileRequestBaseValidator and UpdateProfileValidator for profile validation

---
+++ asp_api1/Application/Validators/UpdateProfileValidator.cs
@@ -0,0 +1,6 @@
+using App.Application.DTOs.Profile;
+using App.Application.Validators.Base;
+
+namespace App.Application.Validators;
+
+public class UpdateProfileValidator : ProfileRequestBaseValidator<UpdateProfileRequestDto> { }
